571a60bf13448f9f772d09339fcb31ef301b1afa
HamzaUmer <mirzasahab123321@gmail.com>
2021-10-05 18:51:54 +0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
    'secretKey': '12345-67890-09876-54321',
-   'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+   'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+   'facebook' : {
+      clientId: '832654344080332',
+      clientSecret: '3c7b6d7dc5fc34820719a6e76e20a13c'
+   }
 }
